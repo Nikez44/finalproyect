c87ed542b8f5f920938f892b005568fc4f3772f5
Kev' Pacheco <kev.pachec.lis@gmail.com>
2015-11-28 10:25:59 -0600

    Arreglado el tamaño

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,13 +4,21 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.0",
-    "cordova-plugin-geolocation": "1.0.1"
+    "cordova-plugin-geolocation": "1.0.1",
+    "plugin.google.maps": "1.3.4"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/maps.js
+++ platforms/android/assets/www/js/maps.js
@@ -2,10 +2,22 @@
  * Created by USER on 27/11/2015.
  */
 
-document.addEventListener("onload", init, false);
+var map;
+document.addEventListener("deviceready", function() {
+    var div = document.getElementById("map");
 
+    // Initialize the map view
+    map = plugin.google.maps.Map.getMap(div);
 
-function init(){
-    $('#map').html("Hola Mundo!");
+    // Wait until the map is ready status.
+    map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+}, false);
+
+function onMapReady() {
+    var button = document.getElementById("button");
+    button.addEventListener("click", onBtnClicked, false);
 }
 
+function onBtnClicked() {
+    map.showDialog();
+}
