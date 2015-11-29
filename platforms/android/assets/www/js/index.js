f6c8f3aee1f2ac8472340cf93ada9c46037bb256
Nikez44 <nike-o_94@hotmail.com>
2015-11-29 17:51:48 -0600

    Se creo la base de datos y recupera los mapas para mostrarlos en el listView

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,96 +1,108 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.initMap();
-    },
+/**
+ * Created by USER on 27/11/2015.
+ */
+document.addEventListener("deviceready", init, false);
 
-    // Update DOM on a Received Event
-    initMap: function() {
-        navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
-    },
+var CENTER;
+var map;
+var db;
 
-    onSuccess: function (position){
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
+function init(){
 
-        var myLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+    createDataBase();
+    fillListView();
+    $(document).on("pageshow", '#maps', initPageMaps);
+}
 
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            scrollwheel: true,
-            zoom: 10
+function createDataBase(){
+    db = openDatabase('dpmaps', '1.0', 'BD de Mapas', 2 * 1024 * 1024);
+
+    db.transaction(function (tx) {
+        tx.executeSql('DROP TABLE IF EXISTS maps');
+        tx.executeSql('DROP TABLE IF EXISTS Markers');
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS maps (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ' +
+            'name VARCHAR(45), ' +
+            'latitud DOUBLE,' +
+            'longitud DOUBLE,' +
+            'zoom INTEGER)');
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS markers (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ' +
+            'title VARCHAR(45), ' +
+            'latitud DOUBLE,' +
+            'longitud DOUBLE,' +
+            'visited TINYINT(1),' +
+            'map_id INTEGER)');
+
+        tx.executeSql("INSERT INTO maps (name, latitud, longitud, zoom) VALUES ('Mapa 1', '3.57', '4.44', '10')");
+        tx.executeSql("INSERT INTO maps (name, latitud, longitud, zoom) VALUES ('Mapa 2', '23.57', '38.44', '10')");
+        tx.executeSql("INSERT INTO markers (title, latitud, longitud, visited, map_id) VALUES ('Lugar 1', '5.57', '10.44', '1', '1')");
+    });
+}
+
+function fillListView(){
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM maps', [], function (tx, result) {
+            for (var i = 0; i < result.rows.length; i++) {
+                var item = result.rows.item(i);
+                var element = $('<li><a href="#" class="ui-btn waves-effect waves-button waves-effect waves-button waves-effect waves-button"><h2>'+item.name+'</h2><p> Latitud: ' + item.latitud + ' Longitud: ' + item.longitud +'</p></a></li>');
+                $('#maps-list').append(element);
+            }
         });
+    });
 
-        map.addListener('click', function(e) {
-            placeMarkerAndPanTo(e.latLng, map);
-        });
 
-        function placeMarkerAndPanTo(latLng, map) {
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-            });
-            map.panTo(latLng);
+}
+
+
+/**
+ * Funciones para la pagina de MAPS
+ */
+function initPageMaps(){
+    navigator.geolocation.getCurrentPosition(drawMap, onErrorGeolocation);
+}
+
+function drawMap(position){
+
+    CENTER = new plugin.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var div = document.getElementById("map");
+
+    // Initialize the map view
+    map = plugin.google.maps.Map.getMap(div, {
+        camera: {
+            latLng: CENTER,
+            zoom: 13
         }
+    });
 
-        var image = 'img/marker_514.png';
-        // Create a marker and set its position.
-        var marker = new google.maps.Marker({
-            map: map,
-            position: myLatLng,
-            title: 'Hello World!',
-            label: 'Hello World!',
-            icon: image
-        });
+    map.addEventListener(plugin.google.maps.event.MAP_READY, initMarker);
 
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">'+ marker.title +'</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
+    map.on(plugin.google.maps.event.MAP_LONG_CLICK, function(latLng) {
+        //alert("Map was long clicked.\n" + latLng.toUrlValue());
+        drawMarker(latLng);
+    });
 
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
-    },
+}
 
-    onError: function(error){
-        alert('code: ' + error.code + '\n' +'message: ' + error.message + '\n');
-    }
-};
+function initMarker(){
+    map.addMarker({
+        position: CENTER,
+        title: "Hello GoogleMap for Cordova!",
+        icon: 'green'
+    });
+}
 
-app.initialize();
+function drawMarker(pos){
+    map.addMarker({
+        position: pos,
+        title: "Hello GoogleMap for Cordova!"
+    });
+}
+
+function onErrorGeolocation(error){
+    alert('code: ' + error.code + '\n' +'message: ' + error.message + '\n');
+}
+
+
