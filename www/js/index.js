d58905d3d5ecaf0c41ddf1508eb1814eb0df35c7
Joshua Flores Tuz <josshft@gmail.com>
2015-12-11 10:02:38 -0600

    [IMPLEMENTACIÓN] Perfil

--- www/js/index.js
+++ www/js/index.js
@@ -15,6 +15,7 @@ var UNITS = 0.00009; //10 metros
 function init(){
     createDataBase();
 	setUserData();
+	setDeviceData();
     initCamera();
 
     fillListView();
@@ -70,7 +71,9 @@ function createDataBase(){
 			'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ' +
 			'name VARCHAR(100), ' +
 			'email VARCHAR(100),' +
-			'nationality VARCHAR(45)' +
+			'nationality VARCHAR(45),' +
+			'blood VARCHAR(5),' +
+			'img TEXT' +
 			');');
 
         tx.executeSql('CREATE TABLE IF NOT EXISTS markers (' +
@@ -109,8 +112,7 @@ function createDataBase(){
         tx.executeSql("INSERT INTO markers (title, latitud, longitud, range, visited, map_id) VALUES ('Lugar 11', '40.7145', '-74.0009', '90', '0', '4')");
         tx.executeSql("INSERT INTO markers (title, latitud, longitud, range, visited, map_id) VALUES ('Lugar 12', '40.7045', '-74.1009', '10', '0', '4')");
 
-		tx.executeSql("INSERT INTO users (name, email, nationality) VALUES ('Joshua', 'josshft@gmail.com', 'México')");
-
+		tx.executeSql("INSERT INTO users (name, email, nationality, blood, img) VALUES ('Joshua', 'josshft@gmail.com', 'México', 'AB-', 'img/josh.jpg')");
 	});
 }
 
@@ -335,6 +337,7 @@ function drawMarker(){
     });
 }
 
+var userId;
 function setUserData () {
 	db.transaction(function (tx) {
 		tx.executeSql('SELECT * FROM users', [], function (tx, result) {
@@ -344,12 +347,48 @@ function setUserData () {
 					'<span class="subline">'+item.email+'</span>');
 				var nationality = item.nationality;
 			}
+			userId = item.id;
+			$('.profile-photo' ).attr('src', item.img);
 			var divUser = $('#divUser');
+			var divUserName = $('#username');
 			var nationDiv = $('#nationality');
+			$('#nationalityProfile' ).val(nationality);
+			$('#email' ).val(item.email);
+			$('#blood' ).append(item.blood);
 			divUser.append(user);
+			divUserName.val(item.name);
 			nationDiv.append(nationality);
 		});
 	});
+}
+
+function setDeviceData () {
+	$('#platform' ).append(device.platform);
+	$('#model' ).append(device.model);
+	$('#version' ).append(device.version);
+}
+
+function editProfile () {
+	var userName = $('#username' ).val();
+	var email = $('#email' ).val();
+	var nationality = $('#nationalityProfile' ).val();
+
+	console.log(userId);
+	console.log(userName);
+
+	db.transaction(function (tx) {
+		tx.executeSql('UPDATE users SET name=?, email=?, nationality=? WHERE id=?;',["Joshua Flores", "nada@nada.com", "RUso", 1]);
+	});
+
+	var qty = sessionStorage.getItem(userName);
+	if (qty){
+		sessionStorage.removeItem(userName);
+		sessionStorage.setItem(userName,(Number(qty)+Number(pQty)));
+	}
+	else
+		sessionStorage.setItem(pName,pQty);
+
+	location.href = 'index.html';
 }
 
 function onErrorGeolocation(error){
